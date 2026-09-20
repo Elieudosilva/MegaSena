f449de28547d024e11c1fb471c48393a922b1de8
Elieudo Silva <elieudosilva95@gmail.com>
2026-09-20 13:48:21 -0300

    (feat): initial project setup

---
+++ app/src/main/java/co/tiagoaguiar/megasenacomposedev/MainActivity.kt
@@ -0,0 +1,45 @@
+package co.tiagoaguiar.megasenacomposedev
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import co.tiagoaguiar.megasenacomposedev.ui.theme.MegaSenaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MegaSenaTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}
+@Composable
+fun MainApp (){
+    Surface(
+        modifier = Modifier.fillMaxSize()) {
+
+ }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MegaSenaTheme {
+        MainApp()
+    }
+}
