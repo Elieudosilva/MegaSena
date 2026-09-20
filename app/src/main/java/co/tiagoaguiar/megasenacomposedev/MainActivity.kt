b94bb4690797828f1bc060a19aac5c301593b719
Elieudo Silva <elieudosilva95@gmail.com>
2026-09-20 14:33:27 -0300

    feat: adicionar mensagem, campo de número e botão

--- app/src/main/java/co/tiagoaguiar/megasenacomposedev/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/megasenacomposedev/MainActivity.kt
@@ -3,12 +3,19 @@ package co.tiagoaguiar.megasenacomposedev
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import co.tiagoaguiar.megasenacomposedev.ui.theme.MegaSenaTheme
 
@@ -17,22 +24,35 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MegaSenaTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                }
+               MainApp()
             }
         }
     }
 }
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainApp (){
     Surface(
-        modifier = Modifier.fillMaxSize()) {
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+        ){
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Boa Sorte!")
 
- }
+            TextField(
+                value = "Digite um número entre 6 e 15",
+                onValueChange = {
+
+                }
+            )
+            Button(onClick = {}) {
+                Text("Gerar números")
+            }
+        }
+    }
 }
 
 
